8549f1ae096fc6e9733d34c012827377295b8042
Kamasu Paul <kamasupaul@gmail.com>
2022-06-05 22:57:12 +0300

    -- add kamasupaul.com in footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -83,7 +83,7 @@
       :absolute="!fixed"
       app
     >
-      <span>&copy; {{ new Date().getFullYear() }}</span>
+      <span>&copy; {{ new Date().getFullYear() }} <a href="https://kamasupaul.com">Kamasu Paul</a></span>
     </v-footer>
   </v-app>
 </template>
